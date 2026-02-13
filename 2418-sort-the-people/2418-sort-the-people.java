d2ea427ce607289c8ae112bddaa22efb992f62bc
Sanjana <sanjanathananjayan@gmail.com>
2026-02-13 20:29:24 +0530

    Time: 23 ms (20.69%), Space: 47.3 MB (61.08%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        int n = names.length;
+        
+        for(int i = 0; i < n - 1; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(heights[i] < heights[j]) {
+                    
+                    
+                    int tempH = heights[i];
+                    heights[i] = heights[j];
+                    heights[j] = tempH;
+                 
+                    String tempN = names[i];
+                    names[i] = names[j];
+                    names[j] = tempN;
+                }
+            }
+        }
+        
+        return names;
+    }
+}
